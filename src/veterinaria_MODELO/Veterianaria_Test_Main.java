9ce2d41c29c10323baa39bba587786713572feb5
Barbi Borrello <biborrello@hotmail.com>
2022-05-26 21:26:04 -0300

    try catch  MAIN
    para realizar pruebas en el main

--- src/veterinaria_MODELO/Veterianaria_Test_Main.java
+++ src/veterinaria_MODELO/Veterianaria_Test_Main.java
@@ -5,6 +5,15 @@
  */
 package veterinaria_MODELO;
 
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import veterinaria_CONTROLADOR.ClienteData;
+import veterinaria_CONTROLADOR.Conexion;
+import veterinaria_CONTROLADOR.MascotaData;
+import veterinaria_CONTROLADOR.TratamientoData;
+import veterinaria_CONTROLADOR.VisitaData;
+
 /**
  *
  * @author Barbara
@@ -16,6 +25,23 @@ public class Veterianaria_Test_Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
+        
+                try {
+            // TODO code application logic here
+            Conexion conexion = new Conexion();
+            ClienteData cd = new ClienteData(conexion);
+            MascotaData md = new MascotaData(conexion);
+            TratamientoData td = new TratamientoData(conexion);
+            VisitaData vd = new VisitaData(conexion);
+            
+         
+
+        
+    }   catch (ClassNotFoundException ex) {
+            Logger.getLogger(Veterianaria_Test_Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+ 
+    
+    } 
     
 }
