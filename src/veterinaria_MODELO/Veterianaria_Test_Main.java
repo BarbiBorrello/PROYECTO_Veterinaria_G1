2e08bbe8d7190194f0d9c69e8106745ae2013e0b
Ikaroyo <emmanuel1631@gmail.com>
2022-05-29 19:34:34 -0300

    Arreglos minimos

--- src/veterinaria_MODELO/Veterianaria_Test_Main.java
+++ src/veterinaria_MODELO/Veterianaria_Test_Main.java
@@ -41,7 +41,7 @@ public class Veterianaria_Test_Main {
 Cliente cliente1 = new Cliente(12345678, "El", "Chespirito", "Avenida siempre viva 123", 12345678, "Pedrito rulito",true);
 cd.agregarCliente(cliente1);
 
-Mascota mascota1 = new Mascota("Pedro", "macho", "perro", "golden", "dorado", LocalDate.of(2020,4, 29), 40, 50.5, true, cliente1);
+Mascota mascota1 = new Mascota("Pedro", "macho", "perro", "golden", "dorado", LocalDate.of(2020,4, 29), 40, 50.5, true, cd.buscarCliente(2));
 md.agregar_Mascota(mascota1);
 //
 //Mascota mascota12 = new Mascota("Pedro", "macho", "perro", "golden", "dorado", LocalDate.of(2020,4, 29), 40, 50.5, true, cliente1);
