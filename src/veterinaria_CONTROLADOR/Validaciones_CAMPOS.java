ce864bca43dedefcfc6893e116ef2f53456b09a6
Barbi Borrello <biborrello@hotmail.com>
2022-06-11 21:29:52 -0300

    Update Validaciones_CAMPOS.java

--- src/veterinaria_CONTROLADOR/Validaciones_CAMPOS.java
+++ src/veterinaria_CONTROLADOR/Validaciones_CAMPOS.java
@@ -5,10 +5,63 @@
  */
 package veterinaria_CONTROLADOR;
 
+import java.awt.Toolkit;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.JTextField;
+
 /**
  *
  * @author Barbara
  */
-public class Validaciones_CAMPOS {
+
+    
+    public class Validaciones_CAMPOS {
+    
+    public static void SNumero(JTextField a){
+        a.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                char c = e.getKeyChar();
+                if(Character.isLetter(c)){
+                    Toolkit.getDefaultToolkit().beep();
+                    e.consume();
+                }
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+            }
+        });
+    }
+    
+    public static void SLetras(JTextField a){
+        a.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                char c=e.getKeyChar();
+                if(Character.isDigit(c)){
+                    Toolkit.getDefaultToolkit().beep();
+                    e.consume();
+                }
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+            }
+        });
+    }
     
 }
