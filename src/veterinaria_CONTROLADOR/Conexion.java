f3c9f2e3a255fb6223acb2d822bf544f61b02e97
Barbi Borrello <biborrello@hotmail.com>
2022-07-07 19:35:42 -0300

    Update Conexion.java

--- src/veterinaria_CONTROLADOR/Conexion.java
+++ src/veterinaria_CONTROLADOR/Conexion.java
@@ -16,7 +16,7 @@ import java.sql.SQLException;
 public class Conexion {
     
     
-    private String url = "jdbc:mysql://localhost/veterinariag1";
+    private String url = "jdbc:mysql://localhost/veterinaria_el_jaguel";
     private String usuario = "root";
     private String password = "";
 
